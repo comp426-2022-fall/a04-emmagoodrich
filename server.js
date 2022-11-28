1abb9d37deadd5d39367e8a412270a39f6007b25
Emma Goodrich <emmaliz@email.unc.edu>
2022-11-28 16:47:55 -0500

    fixed output to be json

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const args = minimist(process.argv.slice(2))
 const port = args.port || 5000
 
 app.listen(port, () => {
-	console.log("Server listening on port " + port + ".")
+	// console.log("Server listening on port " + port + ".")
 })
 
 app.use(express.json());
@@ -21,20 +21,20 @@ app.get('/app/', (req, res, next) => {
 
 //Endpoint /app/roll
 app.post('/app/roll', (req, res, next) => {
-    console.log(roll(6, 2, 1));
+    console.log(JSON.stringify(roll(6, 2, 1)));
 })
 
 //Endpoint /app/roll/:sides/
 app.get('app/roll/:sides', (req, res, next) => {
     let numSides = req.params.sides;
-    console.log(roll(numSides, 2, 1));
+    console.log(JSON.stringify(roll(numSides, 2, 1)));
 })
 
 //Endpoint /app/roll/:sides/:dice/
 app.post('app/roll/:sides/:dice/', (req, res, next) => {
     let numSides = req.params.sides;
     let numDice = req.params.dice;
-    console.log(roll(numSides, numDice, 1));
+    console.log(JSON.stringify(roll(numSides, numDice, 1)));
 })
 
 //Endpoint /app/roll/:sides/:dice/:rolls
@@ -42,5 +42,5 @@ app.post('app/roll/:sides/:dice/:rolls', (req, res, next) => {
     let numSides = req.params.sides;
     let numDice = req.params.dice;
     let numRolls = req.params.rolls;
-    console.log(roll(numSides, numDice, numRolls));
+    console.log(JSON.stringify(roll(numSides, numDice, numRolls)));
 })
