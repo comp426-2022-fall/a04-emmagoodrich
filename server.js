819b8f2a2aef3b3b606b997b5f8a1f5ebe3e63b1
Emma Goodrich <emmaliz@email.unc.edu>
2022-11-28 19:18:12 -0500

    changed variable names to be different from imported module

--- server.js
+++ server.js
@@ -8,47 +8,19 @@ const args = minimist(process.argv.slice(2))
 
 const port = args.port || 5000
 
-app.listen(port, () => {
-	// console.log("Server listening on port " + port + ".")
-})
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/app/', (req, res, next) => {
+app.get('/app/', (req, res) => {
     res.status(200).send('200 OK');
 }) 
 
 //Endpoint /app/roll
-app.get('/app/roll', (req, res, next) => {
-    let numSides = 6;
-    let numDice = 2;
-    let numRolls = 1;
-    res.status(200).send(roll(numSides, numDice, numRolls));
-})
-
-//Endpoint /app/roll/:sides/
-app.get('app/roll/:sides', (req, res, next) => {
-    let numSides = parseInt(req.params.sides);
-    let numDice = 2;
-    let numRolls = 1;    
-    res.status(200).send(roll(numSides, numDice, numRolls));
-})
-
-//Endpoint /app/roll/:sides/:dice/
-app.get('app/roll/:sides/:dice/', (req, res, next) => {
-    let numSides = parseInt(req.params.sides);
-    let numDice = parseInt(req.params.dice);
-    let numRolls = 1;
-    res.status(200).send(roll(numSides, numDice, numRolls));
-})
-
-//Endpoint /app/roll/:sides/:dice/:rolls
-app.get('app/roll/:sides/:dice/:rolls', (req, res, next) => {
-    let numSides = parseInt(req.params.sides);
-    let numDice = parseInt(req.params.dice);
-    let numRolls = parseInt(req.params.rolls);
-    res.status(200).send(roll(numSides, numDice, numRolls));
+app.get('/app/roll', (req, res) => {
+    let sides = 6;
+    let dice = 2;
+    let rolls = 1;
+    res.status(200).send(roll(sides, dice, rolls));
 })
 
 app.post('/app/roll/', (req, res) => {
@@ -58,8 +30,35 @@ app.post('/app/roll/', (req, res) => {
     res.status(200).send(roll(sides, dice, rolls));
 })
 
+//Endpoint /app/roll/:sides/
+app.get('app/roll/:sides/', (req, res) => {
+    let sides = parseInt(req.params.sides);
+    let dice = 2;
+    let rolls = 1;    
+    res.status(200).send(roll(sides, dice, rolls));
+})
+
+//Endpoint /app/roll/:sides/:dice/
+app.get('app/roll/:sides/:dice/', (req, res) => {
+    let sides = parseInt(req.params.sides);
+    let dice = parseInt(req.params.dice);
+    let rolls = 1;
+    res.status(200).send(roll(sides, dice, rolls));
+})
+
+//Endpoint /app/roll/:sides/:dice/:rolls
+app.get('app/roll/:sides/:dice/:rolls/', (req, res) => {
+    let sides = parseInt(req.params.sides);
+    let dice = parseInt(req.params.dice);
+    let rolls = parseInt(req.params.rolls);
+    res.status(200).send(roll(sides, dice, rolls));
+})
+
 //Default endpoint 
 app.get('*', (req, res) => {
-    console.error('error encountered');
     res.status(404).send('404 NOT FOUND');
+})
+
+app.listen(port, () => {
+	// console.log("Server listening on port " + port + ".")
 })
